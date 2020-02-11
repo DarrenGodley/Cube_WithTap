4f869ad7207e0ce4c012bb758e721414c3818b7d
DarrenGodley <32136209+DarrenGodley@users.noreply.github.com>
2020-02-11 10:40:47 +0000

    Initial commit

---
+++ Assets/Controllable.cs
@@ -0,0 +1,48 @@
+﻿﻿using System;
+using UnityEngine;
+
+public class Controllable : MonoBehaviour
+{
+    [SerializeField]
+    private bool bounce = false;
+    private Material _material;
+    private int _direction = 1;
+
+    public bool GetBounce()
+    {
+        return bounce;
+    }
+
+    private void Start()
+    {
+        if (GetComponent<Renderer>() && GetComponent<Renderer>().material)
+        {
+            _material = GetComponent<Renderer>().material;
+        }
+    }
+
+    public void BounceCube()
+    {
+        gameObject.transform.position += Vector3.up * _direction;
+
+        if (gameObject.transform.position.y >= 6.0f)
+        {
+            _direction = -1;
+        }
+
+        else if (gameObject.transform.position.y <= -4.0f)
+        {
+            _direction = 1;
+        }
+    }
+
+    public void ChangeColour(Color newColour)
+    {
+        _material.color = newColour;
+    }
+
+    internal void move(Vector3 offset)
+    {
+        transform.position += offset;
+    }
+}
---
+++ Assets/TouchControl.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchControl : MonoBehaviour
+
+
+{
+    float timer;
+    float MAX_TAP_TIME = 0.2f;
+    bool has_moved = false;
+
+    Camera my_camera = new Camera();
+    // Start is called before the first frame update
+    void Start()
+    {
+        my_camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.touches[0];
+            timer += Time.deltaTime;
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                timer = 0f;
+                has_moved = false;
+            }
+
+            if (touch.phase == TouchPhase.Moved)
+            {
+                has_moved = true;
+            }
+
+            if (touch.phase == TouchPhase.Ended)
+            {
+                if ((timer < MAX_TAP_TIME) && !has_moved)
+                    implement_tap(touch);
+            }
+
+        }
+
+
+
+
+
+
+    }
+
+    private void implement_tap(Touch touch)
+    {
+        Ray our_ray = my_camera.ScreenPointToRay(touch.position);
+
+        RaycastHit info;
+
+        if (Physics.Raycast(our_ray, out info))
+        {
+            Controllable object_hit = info.transform.GetComponent<Controllable>();
+
+            if (object_hit)
+            {
+                object_hit.ChangeColour(Color.magenta);
+            }
+
+        }
+    }
+}
+
+
